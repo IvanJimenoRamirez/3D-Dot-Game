68e1734d70e69629a93bc12c1858839f27fa10ec
IvanJimenoRamirez <ivan.jimeno@estudiantat.upc.edu>
2022-11-20 22:22:17 +0100

    Player movement + attack animation + sound

---
+++ 3D Dot Game/Assets/Scripts/player/PlayerAttacking.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttacking : MonoBehaviour
+{
+    public float attackSpeed = 1.5f;
+    float timeToAttack;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToAttack = 1f / attackSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToAttack -= Time.deltaTime;
+        if (Input.GetMouseButtonDown(0) && timeToAttack <= 0)
+        {
+            timeToAttack = 1f / attackSpeed;
+            // modify the animator to set the "attacking" parameter to true
+            GetComponent<Animator>().SetBool("attacking", true);
+            // invoke the "StopAttacking" function after 0.5 seconds
+            
+            GetComponent<AudioSource>().Play();
+            Invoke("StopAttacking", 0.3f);
+
+        }
+    }
+
+    void StopAttacking()
+    {
+        // modify the animator to set the "attacking" parameter to false
+        GetComponent<Animator>().SetBool("attacking", false);
+    }
+
+    public float getTimeToAttack() {
+        return timeToAttack;
+    }
+}
--- 3D Dot Game/Assets/scripts/player/PlayerMovement.cs
+++ 3D Dot Game/Assets/scripts/player/PlayerMovement.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,63 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool isAttacking = GetComponent<Animator>().GetBool("attacking");
+        // If the player is not attacking and the animation attack has ended then move
+        if (!isAttacking && GetComponent<PlayerAttacking>().getTimeToAttack() <= 0)
+        {
+            bool isMoving = false;
+            // Move the player in the direction of the input and rotate it to face the direction of the input. Taking into account the diagonal character rotation
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                isMoving = true;
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                isMoving = true;
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0, 270, 0);
+                isMoving = true;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(Vector3.forward * speed * Time.deltaTime);
+                transform.rotation = Quaternion.Euler(0, 90, 0);
+                isMoving = true;
+            }
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A))
+            {
+                transform.rotation = Quaternion.Euler(0, 315, 0);
+            }
+            if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+            {
+                transform.rotation = Quaternion.Euler(0, 45, 0);
+            }
+            if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.A))
+            {
+                transform.rotation = Quaternion.Euler(0, 225, 0);
+            }
+            if (Input.GetKey(KeyCode.S) && Input.GetKey(KeyCode.D))
+            {
+                transform.rotation = Quaternion.Euler(0, 135, 0);
+            }
+
+            // modify the animator to set the "moving" parameter to true if the player is moving and false otherwise
+            // Obtain the moving property
+            bool playerAlreadyMoving = GetComponent<Animator>().GetBool("moving");
+            if (isMoving && !playerAlreadyMoving) GetComponent<Animator>().SetBool("moving", isMoving);
+            if (!isMoving && playerAlreadyMoving) GetComponent<Animator>().SetBool("moving", isMoving);
+        }
+        else
+        {
+            // modify the animator to set the "moving" parameter to false
+            GetComponent<Animator>().SetBool("moving", false);
+        }
     }
 }
